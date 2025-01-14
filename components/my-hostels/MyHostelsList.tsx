210c10223b25fbd9ebfd1eb59ff94177af86c827
MgHtetMyatAung <htetmyataung16000@gmail.com>
2025-01-15 02:08:14 +0630

    changes

--- components/my-hostels/MyHostelsList.tsx
+++ components/my-hostels/MyHostelsList.tsx
@@ -5,11 +5,23 @@ import HostelCard from "../hostels/HostelCard";
 
 export default async function MyHostelsList() {
   const session = await getServerSession();
+
+  return (
+    <>{session?.user?.email ? <ListBox email={session.user.email} /> : null}</>
+  );
+}
+
+async function ListBox({ email }: { email?: string }) {
   const userData = await prisma.user.findUnique({
-    where: { email: session?.user?.email! },
+    where: { email: email! },
   });
+
+  return <>{userData?.id ? <HostelsBox id={userData.id} /> : null}</>;
+}
+
+async function HostelsBox({ id }: { id: number }) {
   const hostels = await prisma.hostel.findMany({
-    where: { userId: userData?.id },
+    where: { userId: id },
   });
   return (
     <div className=" space-y-6">
